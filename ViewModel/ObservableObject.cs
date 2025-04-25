0368d542e97028d3fbf332081c7a8fcd594bd90a
SenseiMori <mtpchelokot@gmail.com>
2025-04-25 19:35:03 +0500

    Add submodule ExifLib

--- ViewModel/ObservableObject.cs
+++ ViewModel/ObservableObject.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TextRemoveExif.ViewModel
 {
     public abstract class ObservableObject : INotifyPropertyChanged
