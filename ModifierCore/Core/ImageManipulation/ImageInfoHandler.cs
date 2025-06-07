03e0d6725c956ecb4d47c0bda485a916fde7b234
SenseiMori <mtpchelokot@gmail.com>
2025-06-07 16:27:59 +0500

    newstruct.add core's

---
+++ ModifierCore/Core/ImageManipulation/ImageInfoHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using ExifDeleteLib;
+using SixLabors.ImageSharp;
+using TextRemoveExif.Model.Entities;
+using TextRemoveExif.Model.Interfaces;
+
+
+namespace TextRemoveExif.Services.ImageManipulation
+{
+    public class ImageInfoHandler : IDataProvider
+    {
+        List<byte> exifData = new List<byte>();
+        JPGMetadataReader metadataReader = new JPGMetadataReader();
+        public MyImage GetInfo (string path)
+        {
+            Scaler scaler = new Scaler();
+            exifData = metadataReader.ReadExifFromImage(path);
+            var fileInfo = new FileInfo(path);
+            var imageInfo = Image.Load(path);
+            string exSize = scaler.ConvertToNewSize((imageInfo.Width, imageInfo.Height), Weight.Best);
+            return new MyImage
+
+            {
+                FileName = path,
+                FilePath = System.IO.Path.GetFullPath(path),
+                ShortFileName = System.IO.Path.GetFileNameWithoutExtension(path),
+                Width = imageInfo.Width,
+                Height = imageInfo.Height,
+                WidthHeight = $"{imageInfo.Width}x{imageInfo.Height}",
+                Size = GetBytesReadable(fileInfo.Length),
+                ExpectedWidthHeight = exSize,
+                JPGMarkers = exifData
+                
+            };
+
+        }
+        public string GetBytesReadable(long num)
+        {
+            long absolute_num = (num < 0 ? -num : num);
+            string suffix;
+            double readable;
+            if (absolute_num >= 0x100000) // Megabyte
+            {
+                suffix = "MB";
+                readable = (num >> 10);
+            }
+            else if (absolute_num >= 0x400) // Kilobyte
+            {
+                suffix = "KB";
+                readable = num;
+            }
+            else
+            {
+                return num.ToString("0 B"); // Byte
+            }
+            // Divide by 1024 to get fractional value
+            readable = (readable / 1024);
+            // Return formatted number with suffix
+            return readable.ToString("0.#") + suffix;
+        }
+    }
+}
